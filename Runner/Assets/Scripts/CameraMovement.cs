115eab822b673fbd080ba2e4e4179729d3502fe1
Vlad Lutai <vladyslav.lutai@nure.ua>
2019-05-21 01:08:33 +0300

    Swipe adds

--- Runner/Assets/Scripts/CameraMovement.cs
+++ Runner/Assets/Scripts/CameraMovement.cs
@@ -2,13 +2,20 @@
 
 public class CameraMovement : MonoBehaviour 
 {
+    private const int angleRotZ = 90;
+    private float currentRotZ = -90;
     [Range(0f, 10f)] [SerializeField] private float speed = 0;
     private Vector2 direction = Vector2.up;
     public SwipeController swipeController;
+    [SerializeField] private GameObject character;
 
     private void Update()
     {
-        CheckDirection();
+        if (swipeController.IsSwipe)
+        {
+            CheckDirection();
+            swipeController.IsSwipe = false;
+        }
         transform.Translate(direction * speed * Time.deltaTime);
     }
 
@@ -26,12 +33,15 @@ public class CameraMovement : MonoBehaviour
 
             case Swipe.RIGHT:
                 direction = Vector2.right;
+                currentRotZ -= angleRotZ;
                 break;
 
             case Swipe.LEFT:
                 direction = Vector2.left;
+                currentRotZ += angleRotZ;
                 break;
         }
+        //character.GetComponent<SpriteRenderer>().flipX = true;
     }
 
 }
--- Runner/Assets/Scripts/SwipeController.cs
+++ Runner/Assets/Scripts/SwipeController.cs
@@ -6,6 +6,21 @@ enum Swipe { LEFT, RIGHT, UP, DOWN };
 public class SwipeController : MonoBehaviour, IDragHandler, IBeginDragHandler
 {
     private Swipe currentSwipe = Swipe.UP; // Default
+    private bool isSwipe = false;
+    private const int swipeRange = 0;
+    private const float swipeDistance = 300f;
+    public UnityEngine.UI.Text button;
+    public bool IsSwipe
+    {
+        get
+        {
+            return isSwipe;
+        }
+        set
+        {
+            isSwipe = value;
+        }
+    }
 
     internal Swipe CurrentSwipe
     {
@@ -17,28 +32,34 @@ public class SwipeController : MonoBehaviour, IDragHandler, IBeginDragHandler
 
     void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
     {
-        if(eventData.delta.x > 0) // Right
+        if (eventData.delta.sqrMagnitude > swipeDistance)
         {
-            currentSwipe = Swipe.RIGHT;
-        }
-        if(eventData.delta.x < 0) // Left
-        {
-            currentSwipe = Swipe.LEFT;
-        }
-        if(eventData.delta.y > 0) // Up
-        {
-            currentSwipe = Swipe.UP;
-        }
-        if(eventData.delta.y < 0) // Down
-        {
-            currentSwipe = Swipe.DOWN;
-        }
+            if (Mathf.Abs(eventData.delta.x) > Mathf.Abs(eventData.delta.y))
+            {
+                if (eventData.delta.x > swipeRange) // Right
+                {
+                    currentSwipe = Swipe.RIGHT;
+                }
+                else if (eventData.delta.x < swipeRange)// Left
+                {
 
+                    currentSwipe = Swipe.LEFT;
+                }
+            }
+            else
+            {
+                if (eventData.delta.y > swipeRange) // Up
+                {
+                    currentSwipe = Swipe.UP;
+                }
+                else if (eventData.delta.y < swipeRange)// Down
+                {
+                    currentSwipe = Swipe.DOWN;
+                }
+            }
+            isSwipe = true;
+        }
     }
-
-    void IDragHandler.OnDrag(PointerEventData eventData)
-    {
-
-    }
+    void IDragHandler.OnDrag(PointerEventData eventData) { }
 
 }
